e2f79c22dde2bddf20f3796012c4bff93cc83f37
matmar435 <matmar435@gmail.com>
2025-07-15 21:52:09 +0200

    version1

---
+++ Calculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using calculator;
+
+
+namespace calculator
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+            
+        }
+        private string Podaj()
+        {
+            Console.WriteLine("Podaj liczbę :");
+            string x = Console.ReadLine();
+
+            return x;
+            
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Jaką operację chcesz wykonać?\n1-Dodawanie\n2-Odejmowanie\n3-Mnożenie\n4-Dzielenie");
+
+            var operacja = Console.ReadLine();
+
+            Oblicz oblicz = new Oblicz();
+
+            string x = Podaj();
+            string y = Podaj();
+
+            switch (operacja)
+            {
+                
+                case "1":                    
+                    Console.WriteLine(oblicz.Dodaj(x, y));
+                    break;
+                case "2":
+                    Console.WriteLine(oblicz.Odejmij(x, y));
+                    break;
+
+                case "3":
+                    Console.WriteLine(oblicz.Mnóż(x, y));
+                    break;
+
+                case "4":
+                    Console.WriteLine(oblicz.Dziel(x, y));
+                    break;
+            }
+        }
+    }
+}
